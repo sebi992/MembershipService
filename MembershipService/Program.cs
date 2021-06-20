9d46c03fbc669cae79aa7c75a7fd60f8e0e458b2
sebi992 <85256501+sebi992@users.noreply.github.com>
2021-06-20 14:24:08 +0300

    Initial Commit

---
+++ MembershipService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MembershipService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
